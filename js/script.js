e99dc0d6ef426eac54f0bd0d66ad2f3add23fb97
Luiz Gustavo Santos <santosluizgustavo101@gmail.com>
2020-05-13 21:55:18 -0300

    adcionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from './modules/menu-mobile.js';
 import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBiticoin from './modules/fetch-bitcoin.js';
+import SlideNav from './modules/slide.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -37,5 +38,9 @@ menuMobile.init();
 const funcinamento = new Funcionamento('[data-semana]', 'aberto');
 funcinamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBiticoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
